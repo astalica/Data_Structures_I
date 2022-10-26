ecf5194c15816bce17f882254b92f95e90b0d108
astalica <113649396+astalica@users.noreply.github.com>
2022-10-25 20:11:45 -0400

    Create AccountHolder.java

---
+++ Lab7/AccountHolder.java
@@ -0,0 +1,17 @@
+package Lab07;
+public class AccountHolder 
+{
+	public String username;
+	public String password;
+	public String fullName;
+
+	//-------------------------------------------------
+	public AccountHolder(String un, String pw, String fn)
+	{
+		username = un;
+		password = pw;
+		fullName = fn;
+	}
+
+	
+}
